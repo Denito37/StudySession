e3dd1d0fe1d34171b8d643b0cf44f6be762f1b1e
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-01-19 17:45:59 -0500

    intial table schema & reponse model added

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,18 @@
-import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+url = 'sqlite:///./QA.db'
+
+engine = create_engine(url)
+
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class Questions(Base):
+    __tablename__= 'Questions'
+    id = Column(Integer, primary_key=True)
+    question = Column(String(200))
+
+Base.metadata.create_all(engine)
--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,12 @@
 from pydantic import BaseModel
+
+class Questions_request(BaseModel):
+    question: str
+    topic: str
+    sub_topic:str
+
+class Questions_reponse(BaseModel):
+    id: int
+    question: str
+    topic: str
+    sub_topic:str
