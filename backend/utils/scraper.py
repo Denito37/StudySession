cac0b57a68205ef1618facb6379046c6cc24987a
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-02-02 07:54:28 -0500

    web scraper script started

--- backend/utils/scraper.py
+++ backend/utils/scraper.py
@@ -1,2 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+
+def request_site(url : str) -> str:
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    content = soup.find('div', Class='_qcjzh6')
+    print(f"the content:{content}")
+    return content
+
+def main():
+    url = 'https://cplusplus.com/reference/stl/'
+    url_2 = 'https://www.khanacademy.org/math/statistics-probability/probability-library'
+    request_site(url_2)
+
+
+if __name__ == '__main__':
+    main()
