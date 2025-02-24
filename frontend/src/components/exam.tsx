999d3ef893b8c323e54b86983096a21bc14e5613
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-02-24 11:19:42 -0500

    folder move

--- frontend/src/components/exam.tsx
+++ frontend/src/components/exam.tsx
@@ -9,7 +9,6 @@ export default function Exam({exam}:{exam:Exam}){
     return(
         <>
             <section>
-                
                 <h3>Topic: <span>{exam.topic}</span></h3>
                 <h3>SubTopic: {exam.sub_topic}</h3>
                 <p>Question: {exam.question}</p>
